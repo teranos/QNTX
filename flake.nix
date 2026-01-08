84977095c1acd20a0338dfb1558f4062adab7e63
Brandon <1739143+teranos@users.noreply.github.com>
2026-01-08 03:02:12 +0100

    Add generate-proto Nix app for reproducible protobuf generation (#238)

--- flake.nix
+++ flake.nix
@@ -452,6 +452,24 @@
               ./result/bin/typegen check
             '');
           };
+
+          generate-proto = {
+            type = "app";
+            program = toString (pkgs.writeShellScript "generate-proto" ''
+              set -e
+              echo "Generating gRPC code from proto files..."
+
+              # Use protoc from nixpkgs with Go plugins
+              ${pkgs.protobuf}/bin/protoc \
+                --plugin=${pkgs.protoc-gen-go}/bin/protoc-gen-go \
+                --plugin=${pkgs.protoc-gen-go-grpc}/bin/protoc-gen-go-grpc \
+                --go_out=. --go_opt=paths=source_relative \
+                --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+                plugin/grpc/protocol/domain.proto
+
+              echo "✓ Proto files generated in plugin/grpc/protocol/"
+            '');
+          };
         };
       }
     );
