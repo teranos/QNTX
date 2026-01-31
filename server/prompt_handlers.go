65ff435412157d35ed40688e1968c594d7ffccc0
Brandon van Houten <brandon.vhouten@gmail.com>
2026-02-01 00:36:08 +0100

    Fix missing errors import in server/prompt_handlers.go
    The refactoring removed the errors package import but still uses errors.Wrap

--- server/prompt_handlers.go
+++ server/prompt_handlers.go
@@ -14,6 +14,7 @@ import (
 	"github.com/teranos/QNTX/ats/so/actions/prompt"
 	"github.com/teranos/QNTX/ats/storage"
 	"github.com/teranos/QNTX/ats/types"
+	"github.com/teranos/QNTX/errors"
 	"github.com/teranos/QNTX/logger"
 )
 
