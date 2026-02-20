a4753a15228351f6d49158a9c7d6bce1608edf19
Brandon van Houten <brandon.vhouten@gmail.com>
2026-02-20 17:56:46 +0100

    Update Nix vendorHash after go.sum change

--- flake.nix
+++ flake.nix
@@ -144,7 +144,7 @@
 
           # Hash of vendored Go dependencies (computed from go.sum)
           # To update: set to `pkgs.lib.fakeHash`, run `nix build .#qntx`, copy the hash from error
-          vendorHash = "sha256-Ix5m8m578Jj5mEUy/K1zSWy/wJK9zBO9bGHacPrwOoA=";
+          vendorHash = "sha256-8SQTmPih1ALPjFM6a6zM4o1Bfi/47H9n7NIdDqxFXg0=";
 
           # sqlite3.h needed by sqlite-vec CGO bindings (db/connection.go)
           buildInputs = [ pkgs.sqlite ];
