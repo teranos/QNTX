47808641fe971627a776fcf55e94c13f834e65d3
Brandon van Houten <brandon.vhouten@gmail.com>
2026-02-21 12:54:42 +0100

    Drop vendor directory — go.mod is the source of truth

--- vendor/github.com/go-webauthn/webauthn/protocol/webauthncose/types.go
+++
@@ -1,7 +0,0 @@
-package webauthncose
-
-import "math/big"
-
-type ECDSASignature struct {
-	R, S *big.Int
-}
--- vendor/github.com/tliron/kutil/util/signal_windows.go
+++
@@ -1,9 +0,0 @@
-//go:build windows
-
-package util
-
-import (
-	"os"
-)
-
-var shutdownSignals = []os.Signal{os.Interrupt}
