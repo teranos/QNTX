f917f76f1cc839dd5329a8be40426e7e86581dab
Brandon van Houten <brandon.vhouten@gmail.com>
2026-03-09 17:55:50 +0100

    Add TODO(#645) markers for vanity-id retirement

--- ats/types/id_noqntx.go
+++ ats/types/id_noqntx.go
@@ -7,6 +7,7 @@ import (
 )
 
 // generateASUID falls back to vanity-id when WASM is not available.
+// TODO(#645): remove this file once all callers use qntxwasm and vanity-id is retired.
 func generateASUID(_, subject, predicate, context string) (string, error) {
 	return id.GenerateASID(subject, predicate, context, "")
 }
